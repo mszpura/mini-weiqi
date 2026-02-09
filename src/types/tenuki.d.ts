629d53514a20fce8f6adb5b4fba105ad6096f503
Szpur <maciej.szpura@goelett.com>
2026-02-09 11:31:49 +0100

    Update tsconfig to include .d.ts files and add tenuki type definitions

---
+++ src/types/tenuki.d.ts
@@ -0,0 +1,28 @@
+declare module 'tenuki' {
+	type StoneColor = 'black' | 'white'
+
+	type TenukiHooks = {
+		handleClick?: (y: number, x: number) => void
+		hoverValue?: (y: number, x: number) => StoneColor | undefined
+		gameIsOver?: () => boolean
+	}
+
+	type GameOptions = {
+		element?: HTMLElement
+		boardSize?: number
+		_hooks?: TenukiHooks
+	}
+
+	type GameRenderer = {
+		computeSizing?: () => void
+	}
+
+	export class Game {
+		constructor(options?: GameOptions)
+		renderer?: GameRenderer
+		playAt(y: number, x: number): boolean
+		isOver(): boolean
+		isIllegalAt(y: number, x: number): boolean
+		currentPlayer(): StoneColor
+	}
+}
